819a0fd46899803518633931c66ad69fef97af64
Abdullah Bakir <abdullah.bakir.204@gmail.com>
2026-01-21 14:52:41 +0100

    add env file

--- services/users/users/settings.py
+++ services/users/users/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*fph=uf3!x7kdv7_$hi1ir0vs3ir6)klbmk3+a(dg&czp$hxzu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'true')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", [])
 
 
 # Application definition
